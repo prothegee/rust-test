daf82431f09f1520a2275506c9912dcf24ac2e13
prothegee <prothegee@gmail.com>
2021-03-05 22:41:22 +0700

    Need to understand matching sys in Rust.

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,12 +1,13 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 
 //
 fn main()
 {
 
-    println!("Guess the numba!!");
+    println!("Read the numba!!");
     println!("Range is from 0 to 9");
 
     /*
@@ -18,22 +19,44 @@ fn main()
     //      |name   |type   |bound
     let secret_numba = rand::thread_rng().gen_range(0, 9);
 
-    println!("The secret number is: {}", secret_numba);
-    println!("Now input it again");
+    println!("The secret numba is: {}", secret_numba);
+    println!("So what is the numba?");
 
 
-    let mut guess = String::new();
+    // let's match it
+    loop
+    {
+        let mut guess = String::new();
 
-    // using Rust standard library
-    io::stdin()
+        // using Rust standard library
+        io::stdin()
 
-        // handling the input
-        .read_line(&mut guess)
+            // handling the input
+            .read_line(&mut guess)
 
-        // handling potential failure
-        .expect("Failed to read line");
+            // handling potential failure
+            .expect("Failed to read line");
 
-    // familiar with this formaat?
-    println!("Yo numba is: {}", guess);
+        // handling invalid input
+        // let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = match guess.trim().parse()
+        {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // familiar with this formaat?
+        println!("Yo numba is: {}", guess);
+
+        match guess.cmp(&secret_numba)
+        {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 
 }
